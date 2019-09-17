f126df152bc391e77d19a51543f64d403f03aaa2
Cleanderson Lins <cleandersonlins@gmail.com>
2019-09-16 23:58:09 -0300

    Initial ray casting methods implementation

---
+++ particle.js
@@ -0,0 +1,31 @@
+class Particle{
+    constructor(){
+        this.pos = createVector(width/2, height/2)
+        this.rays = []
+        for (let a = 0; a < 360; a += 1){
+            this.rays.push(new Ray(this.pos, radians(a)))
+        }
+    }
+
+    update(x, y){
+        this.pos.x = x
+        this.pos.y = y
+    }
+
+    look(wall){
+        for(let ray of this.rays) {
+            const pt = ray.cast(wall)
+            if(pt){
+                line(this.pos.x, this.pos.y, pt.x, pt.y)
+            }
+        }
+    }
+
+    show(){
+        fill(255)
+        ellipse(this.pos.x, this.pos.y, 4)
+        for(let ray of this.rays){
+            ray.show()
+        }
+    }
+}
