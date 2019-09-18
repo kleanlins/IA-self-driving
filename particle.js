0bd238b2635f52c8a9444aa99cd985ee6bc997b0
Cleanderson Lins <cleandersonlins@gmail.com>
2019-09-17 23:53:00 -0300

    8 directions as inputs for a NN added

--- particle.js
+++ particle.js
@@ -9,6 +9,7 @@ class Particle{
         for (let a = 0; a < 360; a += 45){
             this.rays.push(new Ray(this.pos, radians(a)))
         }
+        this.brain = new NeuralNetwork(this.rays.length, this.rays.length, 1)
     }
 
     applyForce(force){
@@ -23,11 +24,13 @@ class Particle{
     }
 
     look(walls){
-        for(let ray of this.rays) {
+        const inputs = []
+
+        for(let i = 0; i < this.rays.length; i++) {
             let closest = null
-            let record = Infinity
+            let record = this.sight
             for(let wall of walls){
-                const pt = ray.cast(wall)
+                const pt = this.rays[i].cast(wall)
                 if(pt){
                     const d = p5.Vector.dist(this.pos, pt)
                     if(d < record && d < this.sight){
@@ -36,20 +39,27 @@ class Particle{
                     }
                 }
             }
-            if(record < 2){
-                console.log("hit wall")
-            }
+
+            inputs[i] = map(record, 0, 50, 1, 0)
 
             if(closest){
                 stroke(255, 255, 255, 100)
                 line(this.pos.x, this.pos.y, closest.x, closest.y)
             }
         }
+        const output = this.brain.predict(inputs)
+        const angle = map(output[0], 0, 1, 0, TWO_PI)
+        const steering = p5.Vector.fromAngle(angle)
+        steering.setMag(this.maxSpeed)
+        steering.sub(this.vel)
+        this.applyForce(steering)
+        // console.log(output)
     }
 
+
     show(){
-        fill(255)
-        ellipse(this.pos.x, this.pos.y, 8)
+        fill(255, 100)
+        ellipse(this.pos.x, this.pos.y, 5)
         for(let ray of this.rays){
             // ray.show()
         }
--- sketch.js
+++ sketch.js
@@ -1,27 +1,30 @@
-let ray 
-let wall
-let particle
 let walls = []
 
 let start, end
 
+let population = []
+
 function setup() {
 	createCanvas(800, 800)
-
-	walls.push(new Boundary(50, 700, 50, 400))
+	tf.setBackend('cpu')
+	walls.push(new Boundary(50, 800, 50, 400))
 	walls.push(new Boundary(50, 400, 300, 150))
-	walls.push(new Boundary(300, 150, 700, 150))
+	walls.push(new Boundary(300, 150, 800, 150))
 
 
-	walls.push(new Boundary(150, 700, 150, 400))
+	walls.push(new Boundary(150, 800, 150, 400))
 	walls.push(new Boundary(150, 400, 300, 250))
-	walls.push(new Boundary(300, 250, 700, 250))
+	walls.push(new Boundary(300, 250, 800, 250))
 
 	start = createVector(100, 700)
 	end = createVector(700,200)
 
 	ray = new Ray(100, 200)
-	particle = new Particle(start.x, start.y)
+	
+	for(let i = 0; i < 100; i++){
+		population[i] = new Particle(start.x, start.y)
+	}
+
 }
 
 function draw() {
@@ -29,10 +32,14 @@ function draw() {
 	for(let wall of walls){
 		wall.show()
 	}
-	particle.applyForce(createVector(0, -0.1))
-	particle.update()
-	particle.show()
-	particle.look(walls)
+	
+
+	for(let particle of population){
+		particle.update()
+		particle.show()
+		particle.look(walls)
+	}
+
 	// ray.show()
 	// ray.lookAt(mouseX, mouseY)
 
