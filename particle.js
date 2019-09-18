a9ad641a794d16bcf689250d3f80076863dacf13
Cleanderson Lins <cleandersonlins@gmail.com>
2019-09-17 22:46:22 -0300

    First iteration with 6 sensors

--- particle.js
+++ particle.js
@@ -1,31 +1,57 @@
 class Particle{
-    constructor(){
-        this.pos = createVector(width/2, height/2)
+    constructor(x, y){
+        this.pos = createVector(x, y)
         this.rays = []
-        for (let a = 0; a < 360; a += 1){
+        this.vel = createVector()
+        this.acc = createVector()
+        this.maxSpeed = 3
+        this.sight = 90
+        for (let a = 0; a < 360; a += 45){
             this.rays.push(new Ray(this.pos, radians(a)))
         }
     }
 
-    update(x, y){
-        this.pos.x = x
-        this.pos.y = y
+    applyForce(force){
+        this.acc.add(force)
     }
 
-    look(wall){
+    update(x, y){
+        this.pos.add(this.vel)
+        this.vel.limit(this.maxSpeed)
+        this.vel.add(this.acc)
+        this.acc.set(0, 0)
+    }
+
+    look(walls){
         for(let ray of this.rays) {
-            const pt = ray.cast(wall)
-            if(pt){
-                line(this.pos.x, this.pos.y, pt.x, pt.y)
+            let closest = null
+            let record = Infinity
+            for(let wall of walls){
+                const pt = ray.cast(wall)
+                if(pt){
+                    const d = p5.Vector.dist(this.pos, pt)
+                    if(d < record && d < this.sight){
+                        record = d
+                        closest = pt
+                    }
+                }
+            }
+            if(record < 2){
+                console.log("hit wall")
+            }
+
+            if(closest){
+                stroke(255, 255, 255, 100)
+                line(this.pos.x, this.pos.y, closest.x, closest.y)
             }
         }
     }
 
     show(){
         fill(255)
-        ellipse(this.pos.x, this.pos.y, 4)
+        ellipse(this.pos.x, this.pos.y, 8)
         for(let ray of this.rays){
-            ray.show()
+            // ray.show()
         }
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,20 +1,38 @@
 let ray 
 let wall
 let particle
+let walls = []
+
+let start, end
 
 function setup() {
-	createCanvas(400, 400)
-	wall = new Boundary(300, 100, 300, 300)
+	createCanvas(800, 800)
+
+	walls.push(new Boundary(50, 700, 50, 400))
+	walls.push(new Boundary(50, 400, 300, 150))
+	walls.push(new Boundary(300, 150, 700, 150))
+
+
+	walls.push(new Boundary(150, 700, 150, 400))
+	walls.push(new Boundary(150, 400, 300, 250))
+	walls.push(new Boundary(300, 250, 700, 250))
+
+	start = createVector(100, 700)
+	end = createVector(700,200)
+
 	ray = new Ray(100, 200)
-	particle = new Particle()
+	particle = new Particle(start.x, start.y)
 }
 
 function draw() {
 	background(0)
-	wall.show()
-	particle.update(mouseX, mouseY)
+	for(let wall of walls){
+		wall.show()
+	}
+	particle.applyForce(createVector(0, -0.1))
+	particle.update()
 	particle.show()
-	particle.look(wall)
+	particle.look(walls)
 	// ray.show()
 	// ray.lookAt(mouseX, mouseY)
 
@@ -24,4 +42,6 @@ function draw() {
 	// 	fill(255)
 	// 	ellipse(pt.x, pt.y, 8, 8)
 	// }
+	ellipse(start.x, start.y, 10)
+	ellipse(end.x, end.y, 10)
 }
